d1371f4564ef087649b07637118e1255235a1c74
RompfRobert <rrompf@ibs-b.hu>
2022-03-04 11:23:48 +0100

    upload funcitons

--- JavaScript_Algorithms_and_Data_Structures/JavaScript_basics.js
+++ JavaScript_Algorithms_and_Data_Structures/JavaScript_basics.js
@@ -10,3 +10,38 @@ items = [
 
 console.log(items[1][1])
 console.log(items[3][0])
+
+// FUNCTIONS 
+function functionWithArgs(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(functionWithArgs(1,2));
+
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(timesFive(5));
+
+// GLOBAL SCOPE
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
