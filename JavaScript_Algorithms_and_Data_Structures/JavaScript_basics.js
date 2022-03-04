86e03c2d09a02a72524372cd8c60b17a9a8baca7
RompfRobert <rrompf@ibs-b.hu>
2022-03-04 10:29:45 +0100

    upload JavaScript basics lessons

---
+++ JavaScript_Algorithms_and_Data_Structures/JavaScript_basics.js
@@ -0,0 +1,12 @@
+let items = [];
+
+items = [
+    ["milk", 1],
+    ["bread", 0.9],
+    ["banana", 1.2],
+    ["chicken", 2.5],
+    ["water", 2],
+];
+
+console.log(items[1][1])
+console.log(items[3][0])
